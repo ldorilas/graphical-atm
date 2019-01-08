2af2e08104aa9a57d4253b01021685a442e794c7
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-08 09:21:27 -0500

    Update CreateView.java

--- src/view/CreateView.java
+++ src/view/CreateView.java
@@ -80,6 +80,21 @@ public class CreateView extends JPanel implements ActionListener {
 		this.add(lastNameField);
 	}
 	
+	private void initYearDropdown() {
+		JLabel label = new JLabel("Year", SwingConstants.RIGHT);
+		label.setBounds(100, 100, 95, 35);
+		label.setLabelFor(yearDropdown);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		String[] years = { "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000" };
+		JComboBox<String> dropdown = new JComboBox<String>(years);
+		JComboBox yearDropdown = dropdown;
+		yearDropdown.addActionListener(this);
+		
+		this.add(label);
+		this.add(yearDropdown);
+	}
+	
 	private void initMonthDropdown() {
 		JLabel label = new JLabel("Month", SwingConstants.RIGHT);
 		label.setBounds(100, 100, 95, 35);
@@ -96,19 +111,20 @@ public class CreateView extends JPanel implements ActionListener {
 	}
 	
 	private void initDayDropdown() {
-		JLabel label = new JLabel("Month", SwingConstants.RIGHT);
+		JLabel label = new JLabel("Day", SwingConstants.RIGHT);
 		label.setBounds(100, 100, 95, 35);
-		label.setLabelFor(monthDropdown);
+		label.setLabelFor(dayDropdown);
 		label.setFont(new Font("DialogInput", Font.BOLD, 14));
 		
 		String[] days = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" };
-		JComboBox<String> dropdown = new JComboBox<String>(months);
-		JComboBox monthDropdown = dropdown;
-		monthDropdown.addActionListener(this);
+		JComboBox<String> dropdown = new JComboBox<String>(days);
+		JComboBox dayDropdown = dropdown;
+		dayDropdown.addActionListener(this);
 		
 		this.add(label);
-		this.add(monthDropdown);
+		this.add(dayDropdown);
 	}
+
 	
 	private void initPinField() {
 		JLabel label = new JLabel("PIN", SwingConstants.RIGHT);
@@ -125,16 +141,18 @@ public class CreateView extends JPanel implements ActionListener {
 	}
 	
 	private void initialize() {
-		
-		initFirstNameField();
-		initLastNameField();
-		
+	
 		// TODO
 		//
 		// this is a placeholder for this view and should be removed once you start
 		// building the CreateView.
 		
 		this.add(new javax.swing.JLabel("CreateView", javax.swing.SwingConstants.CENTER));
+		initFirstNameField();
+		initLastNameField();
+		initYearDropdown();
+		initMonthDropdown();
+		initDayDropdown();
 		
 		// TODO
 		//
