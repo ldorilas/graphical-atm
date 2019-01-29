2fe49c8fc10bec4ce1a8655d33172b4c58294c8f
ldorilas <43239931+ldorilas@users.noreply.github.com>
2019-01-28 23:50:05 -0500

    finished

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -5,7 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent; 
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
